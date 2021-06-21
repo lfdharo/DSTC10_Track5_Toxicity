bdcdfbaa8df31830ba61d54c95a9a5f6949ae103
e0397123 <e0397123@u.nus.ed>
2021-06-21 17:11:43 +0800

    modify generation script

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,16 +3,17 @@
 #SBATCH --job-name=run
 #SBATCH -n 1
 #SBATCH --gres=gpu:1
-#SBATCH -w ttnusa3
 #SBATCH -p new
 
-/home/chen/anaconda3/envs/transformers-3.4/bin/python3 run_generation.py \
+PROMPT="[CLS]some stupid vlogger no one cares. I just like to say I don't care about stupid shit personally. Now someone tell me how I must actually care because I'm writing a comment about how I donut care and somehow saying you don't care about something means you actually do care, even though you just want to talk about how you don't care.[SEP]"
+
+python3 run_generation.py \
     --model_type gpt2 \
-    --model_name_or_path gpt2_medium_output \
-	--prompt dailydialog_train_context.txt\
-    --num_return_sequences 1 \
-    --length 128 \
+    --model_name_or_path gpt2_output \
+	--prompt "${PROMPT}" \
+    --num_return_sequences 3 \
+    --length 200 \
     --temperature 1.0 \
-    --stop_token "<|endoftext|>" > output.log 2>&1 & 
+    --stop_token "<|endoftext|>"
 
     
--- scripts/run_generation.py
+++ scripts/run_generation.py
@@ -20,10 +20,10 @@
 
 import argparse
 import logging
-import codecs
+
 import numpy as np
 import torch
-from tqdm import tqdm
+
 from transformers import (
     CTRLLMHeadModel,
     CTRLTokenizer,
@@ -167,7 +167,6 @@ def main():
         required=True,
         help="Path to pre-trained model or shortcut name selected in the list: " + ", ".join(MODEL_CLASSES.keys()),
     )
-    parser.add_argument("--output_file_name", type=str, default="")
 
     parser.add_argument("--prompt", type=str, default="")
     parser.add_argument("--length", type=int, default=20)
@@ -228,73 +227,69 @@ def main():
     args.length = adjust_length_to_model(args.length, max_sequence_length=model.config.max_position_embeddings)
     logger.info(args)
 
-#    prompt_text = args.prompt if args.prompt else input("Model prompt >>> ")
-    with codecs.open(args.prompt, mode='r', encoding='utf-8') as rf:
-        lines = rf.readlines()
-    lines = [l.strip() for l in lines]
+    prompt_text = args.prompt if args.prompt else input("Model prompt >>> ")
+
+    # Different models need different input formatting and/or extra arguments
+    requires_preprocessing = args.model_type in PREPROCESSING_FUNCTIONS.keys()
+    if requires_preprocessing:
+        prepare_input = PREPROCESSING_FUNCTIONS.get(args.model_type)
+        preprocessed_prompt_text = prepare_input(args, model, tokenizer, prompt_text)
+
+        if model.__class__.__name__ in ["TransfoXLLMHeadModel"]:
+            tokenizer_kwargs = {"add_space_before_punct_symbol": True}
+        else:
+            tokenizer_kwargs = {}
+
+        encoded_prompt = tokenizer.encode(
+            preprocessed_prompt_text, add_special_tokens=False, return_tensors="pt", **tokenizer_kwargs
+        )
+    else:
+        prefix = args.prefix if args.prefix else args.padding_text
+        encoded_prompt = tokenizer.encode(prefix + prompt_text, add_special_tokens=False, return_tensors="pt")
+    encoded_prompt = encoded_prompt.to(args.device)
+
+    if encoded_prompt.size()[-1] == 0:
+        input_ids = None
+    else:
+        input_ids = encoded_prompt
+
+    output_sequences = model.generate(
+        input_ids=input_ids,
+        max_length=args.length + len(encoded_prompt[0]),
+        temperature=args.temperature,
+        top_k=args.k,
+        top_p=args.p,
+        repetition_penalty=args.repetition_penalty,
+        do_sample=True,
+        num_return_sequences=args.num_return_sequences,
+    )
+
+    # Remove the batch dimension when returning multiple sequences
+    if len(output_sequences.shape) > 2:
+        output_sequences.squeeze_()
+
     generated_sequences = []
-    for j, prompt_text in enumerate(lines):
-        # Different models need different input formatting and/or extra arguments
-        requires_preprocessing = args.model_type in PREPROCESSING_FUNCTIONS.keys()
-        if requires_preprocessing:
-            prepare_input = PREPROCESSING_FUNCTIONS.get(args.model_type)
-            preprocessed_prompt_text = prepare_input(args, model, tokenizer, prompt_text)
 
-            if model.__class__.__name__ in ["TransfoXLLMHeadModel"]:
-                tokenizer_kwargs = {"add_space_before_punct_symbol": True}
-            else:
-                tokenizer_kwargs = {}
+    for generated_sequence_idx, generated_sequence in enumerate(output_sequences):
+        print("=== GENERATED SEQUENCE {} ===".format(generated_sequence_idx + 1))
+        generated_sequence = generated_sequence.tolist()
 
-            encoded_prompt = tokenizer.encode(
-                preprocessed_prompt_text, add_special_tokens=False, return_tensors="pt", **tokenizer_kwargs
-            )
-        else:
-            prefix = args.prefix if args.prefix else args.padding_text
-            encoded_prompt = tokenizer.encode(prefix + prompt_text, add_special_tokens=False, return_tensors="pt")
-        encoded_prompt = encoded_prompt.to(args.device)
+        # Decode text
+        text = tokenizer.decode(generated_sequence, clean_up_tokenization_spaces=True)
 
-        if encoded_prompt.size()[-1] == 0:
-            input_ids = None
-        else:
-            input_ids = encoded_prompt
+        # Remove all text after the stop token
+        text = text[: text.find(args.stop_token) if args.stop_token else None]
 
-        output_sequences = model.generate(
-            input_ids=input_ids,
-            max_length=args.length + len(encoded_prompt[0]),
-            temperature=args.temperature,
-            top_k=args.k,
-            top_p=args.p,
-            repetition_penalty=args.repetition_penalty,
-            do_sample=True,
-            num_return_sequences=args.num_return_sequences,
+        # Add the prompt at the beginning of the sequence. Remove the excess text that was used for pre-processing
+        total_sequence = (
+            prompt_text + text[len(tokenizer.decode(encoded_prompt[0], clean_up_tokenization_spaces=True)) :]
         )
 
-        # Remove the batch dimension when returning multiple sequences
-        if len(output_sequences.shape) > 2:
-            output_sequences.squeeze_()
+        generated_sequences.append(total_sequence)
+        print(total_sequence)
 
-
-        for generated_sequence_idx, generated_sequence in enumerate(output_sequences):
-            logger.info("=== GENERATED SEQUENCE {} ===".format(j + 1))
-            generated_sequence = generated_sequence.tolist()
-
-            # Decode text
-            text = tokenizer.decode(generated_sequence, clean_up_tokenization_spaces=True)
-
-            # Remove all text after the stop token
-            text = text[: text.find(args.stop_token) if args.stop_token else None]
-
-            # Add the prompt at the beginning of the sequence. Remove the excess text that was used for pre-processing
-            total_sequence = text[len(tokenizer.decode(encoded_prompt[0], clean_up_tokenization_spaces=True)) :]
-
-            generated_sequences.append(total_sequence)
-            logger.info(total_sequence)
-
-    return generated_sequences, args
+    return generated_sequences
 
 
 if __name__ == "__main__":
-    generated_sequences, args  = main()
-    with codecs.open(args.output_file_name, mode='w', encoding='utf-8') as wf:
-        for l in generated_sequences:
-            wf.write(l.strip() + '\n')
+    main()
